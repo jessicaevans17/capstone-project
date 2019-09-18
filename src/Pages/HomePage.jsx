7e4b865e77d18fa6570dfef63208c59953e2ebef
Jessica Evans <jme@mail.usf.edu>
2019-09-18 13:04:39 -0400

    seperated sass files

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Logo from "../images/logo.png"
 import Profile from "../images/profilepic.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faDice } from "@fortawesome/free-solid-svg-icons"
+import { Link } from "react-router-dom"
 const element = <FontAwesomeIcon icon={faDice} />
 const HomePage = () => {
   return (
@@ -31,7 +32,13 @@ const HomePage = () => {
         </nav>
       </header>
       <main>
-        <section className="content"></section>
+        <section className="content">
+          <h1 className="main-prompt">
+            Have a board game you want to play, but no one to play it with?
+          </h1>
+          <Link className="sign-in-button">Start a Game</Link>
+          <Link className="find-a-game-button">Browse Games Near You</Link>
+        </section>
       </main>
       <footer></footer>
     </>
